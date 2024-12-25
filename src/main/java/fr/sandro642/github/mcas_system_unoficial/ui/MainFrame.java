1641181e10cb2b0b61c52b9fc405182adef226e3
Sandro SOria <sandro33810@gmail.com>
2024-12-25 11:08:27 +0100

    Refactor changes

--- src/main/java/fr/sandro642/github/mcas_system_unoficial/ui/MainFrame.java
+++ src/main/java/fr/sandro642/github/mcas_system_unoficial/ui/MainFrame.java
@@ -12,23 +12,18 @@ public class MainFrame extends JFrame {
     private final CommandHandler commandHandler;
     private final TerminalPanel terminalPanel;
     private final StatusPanel statusPanel;
-    private final AirplaneModelPanel airplaneModelPanel;
 
     public MainFrame() {
         this.aircraftState = new AircraftState();
         this.commandHandler = new CommandHandler(aircraftState);
         this.terminalPanel = new TerminalPanel(commandHandler);
         this.statusPanel = new StatusPanel(aircraftState);
-        this.airplaneModelPanel = new AirplaneModelPanel();  // Initialisation du modèle 3D
 
         // Initialisation de la fenêtre
         setTitle("MCAS System");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-
-        // Ajouter le panneau 3D de l'avion
-        add(airplaneModelPanel, BorderLayout.CENTER);
 
         // Ajouter le terminal et le panneau de statut
         add(terminalPanel, BorderLayout.SOUTH);
@@ -38,5 +33,4 @@ public class MainFrame extends JFrame {
     public void init() {
         setVisible(true);
     }
-
 }
