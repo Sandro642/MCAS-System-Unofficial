011315a27f094dccc8729f446d9bc694ad3d8c34
Sandro Soria <sandro33810@gmail.com>
2025-01-08 20:48:19 +0100

    Update dependencies and enhance CLI logging functionality

--- src/MCASMain.ts
+++ src/MCASMain.ts
@@ -1,12 +1,15 @@
 import 'colors';
 import NameApp from './utils/NameApp';
 import initCLI from './cli/MainCli';
+import Logger from './utils/Logger';
+
+const logger = new Logger();
 
 function initMCASMain() {
     console.clear();
     console.log(NameApp.message + "\n\n");
-    console.log("MCAS Updater n'est toujours pas implémenté." + "\n");
-    console.log("Vous pouvez utiliser la commande '-help' pour afficher la liste des commandes disponibles.\n".gray);
+    logger.info("MCAS Updater n'est toujours pas implémenté." + "\n");
+    logger.info("Vous pouvez utiliser la commande '-help' pour afficher la liste des commandes disponibles.\n");
 
     initCLI();
 }
--- src/cli/MainCli.ts
+++ src/cli/MainCli.ts
@@ -1,8 +1,6 @@
 import 'colors';
 import * as readline from 'readline';
-import * as fs from 'fs';
 import Logger from '../utils/Logger';
-import NameApp from '../utils/NameApp';
 
 const logger = new Logger();
 
@@ -17,12 +15,17 @@ function initCLI() {
     rl.prompt();
 
     rl.on('line', (line) => {
-        switch (line.trim()) {
+        const [command, subCommand] = line.trim().split(' ');
+
+        switch (command) {
             case '-help':
-                console.log('Liste des commandes disponibles : help, exit');
+                logger.info('Liste des commandes disponibles : help, exit, updater');
                 break;
             case 'exit':
                 rl.close();
+                break;
+            case 'updater':
+                handleUpdaterCommand(subCommand);
                 break;
             default:
                 logger.info('Commande inconnue. Tapez "-help" pour obtenir de l\'aide.\n');
@@ -31,9 +34,29 @@ function initCLI() {
         rl.prompt();
     }).on('close', () => {
         console.clear();
-        console.log('Au revoir! By Sandro642 MCAS System Unofficial');
+        logger.info('Au revoir! By Sandro642 MCAS System Unofficial');
         process.exit(0);
     });
+}
+
+function handleUpdaterCommand(subCommand: string) {
+    switch (subCommand) {
+        case 'start':
+            logger.info('Updater démarré.');
+            break;
+        case 'stop':
+            logger.info('Updater arrêté.');
+            break;
+        case 'status':
+            logger.info('Statut de l\'updater : en cours.');
+            break;
+        case 'run':
+            logger.info('Updater en cours d\'exécution.');
+            break;
+        default:
+            logger.info('Sous-commande inconnue pour "updater". Utilisez "start", "stop", "status" ou "run".');
+            break;
+    }
 }
 
 export default initCLI;
