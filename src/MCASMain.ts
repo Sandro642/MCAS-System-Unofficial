435f5d0ed21cca9e28b19c8b8f031bc68ae6b4d4
Sandro Soria <sandro33810@gmail.com>
2025-01-10 17:32:15 +0100

    Add some files and new directory

--- src/MCASMain.ts
+++ src/MCASMain.ts
@@ -8,8 +8,8 @@ const logger = new Logger();
 function initMCASMain() {
     console.clear();
     console.log(NameApp.message + "\n\n");
-    logger.info("MCAS Updater n'est toujours pas implémenté." + "\n");
-    logger.info("Vous pouvez utiliser la commande '-help' pour afficher la liste des commandes disponibles.\n");
+    logger.info("MCAS Updater est implémenté ! Veuillez l'utiliser avec <services updater start> !" + "\n");
+    logger.info("Vous pouvez utiliser la commande 'help' pour afficher la liste des commandes disponibles.\n");
 
     initCLI();
 }
--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,18 +1,20 @@
+import 'colors';
+
 function getTimestamp(): string {
     return new Date().toISOString();
 }
 
 class Logger {
     info(message: string): void {
-        console.log(`[${getTimestamp()}] INFO: ${message}`);
+        console.log(`[${getTimestamp()}]`.gray + ` INFO:`.blue + ` ${message}`);
     }
 
     warn(message: string): void {
-        console.warn(`[${getTimestamp()}] WARN: ${message}`);
+        console.warn(`[${getTimestamp()}]`.gray + ` WARN:`.yellow + ` ${message}`);
     }
 
     error(message: string): void {
-        console.error(`[${getTimestamp()}] ERROR: ${message}`);
+        console.error(`[${getTimestamp()}]`.gray + ` ERROR:`.red + ` ${message}`);
     }
 }
 
